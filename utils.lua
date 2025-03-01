ceacb90a429a94d618a64880dee5c4e0498d8d23
Krampus <pedro_br14@hotmail.com>
2025-03-01 17:50:15 -0300

    aumentando modularidade para permitir melhor processamento de outros modelos de ficha

--- utils.lua
+++ utils.lua
@@ -9,4 +9,26 @@ function utils.contains(table, val)
     return false
 end
 
+function utils.processMessage(msg,validDataTypes)
+    
+    local objMesa = msg.room
+    if objMesa == nil then
+        return nil, nil, nil
+    else
+        local meuJogador = objMesa.me
+        local mainPG = objMesa:findBibliotecaItem(meuJogador.personagemPrincipal)
+        if mainPG == nil then
+            return nil, nil, nil
+        else
+            local promise = mainPG:asyncOpenNDB()
+            local sheet = await(promise)
+            if utils.contains(validDataTypes, mainPG.dataType) then
+                return mainPG, meuJogador, sheet
+            else
+                return nil, nil, nil
+            end
+        end
+    end
+end
+
 return utils
